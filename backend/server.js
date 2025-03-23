9e3f816d4b3afe780a3d4f583c4f1d8c20658789
sandrogaliazzi <sroque.roque@gmail.com>
2025-03-23 11:28:17 -0300

    adicionado script de atualização do tomodat

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,15 @@
 import app from "./src/app.js";
 import http from "http";
 import signalUpdateLoop from "./src/scripts/uploadSignals.js";
+import tomodatUpdateLoop from "./src/scripts/updateFetch.js";
 
 const port = process.env.PORT || 5005; //always 5005
 const host = "0.0.0.0";
 
-// var privateKey  = fs.readFileSync('src/key.pem', 'utf8');
-// var certificate = fs.readFileSync('src/cert.pem', 'utf8');
-
-// var credentials = {key: privateKey, cert: certificate,};
-
 const server = http.createServer(app);
 
-// signalUpdateLoop();
+signalUpdateLoop();
+tomodatUpdateLoop();
 
 server.listen(port, () => {
   let now = new Date().toLocaleString("PT-br");
--- backend/src/scripts/updateFetch.js
+++ backend/src/scripts/updateFetch.js
@@ -2,42 +2,49 @@ import newFetchModel from "../models/newetchWithPppoe.js";
 import { newFetchTomodat } from "../scripts/fetchApiTomodat.js";
 import db from "../config/dbConnect.js";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 db.on("error", console.log.bind(console, "erro de conexão"));
-db.once("open", () => {
+db.once("open", async () => {
   let now = new Date().toLocaleString("PT-br");
   console.log(`conexão com o banco em: ${now}`);
 });
 
-async function updateFetch() {
+async function updateFetchInBatches(data) {
   try {
-    const now = new Date().toLocaleString("PT-br");
-    const data = await newFetchTomodat();
-
-    await Promise.all(
-      data.map(async element => {
-        const id = element.id;
-        await newFetchModel.findOneAndUpdate({ id }, element, { upsert: true }); // Update or insert if not found
-      })
-    );
-
-    console.log(`Fetch successfully updated to Db at: ${new Date().toLocaleString("PT-br")}`);
+    const updates = data.map(element => ({
+      updateOne: {
+        filter: { id: element.id },
+        update: element,
+        upsert: true,
+      },
+    }));
+    await newFetchModel.bulkWrite(updates); // Batch update using bulkWrite
   } catch (err) {
     console.error(`Error updating data: ${err.message}`);
   }
 }
 
-async function mainLoop() {
+async function tomodatUpdateLoop() {
   try {
     while (true) {
-      await updateFetch();
+      const now = new Date().toLocaleString("PT-br");
+      const data = await newFetchTomodat();
+      await updateFetchInBatches(data);
+      console.log(`Fetch successfully updated to Db at: ${now}`);
       console.log(
         `Waiting for ${3 * 60 * 1000} milliseconds before next update...`
       );
-      await new Promise(resolve => setTimeout(resolve, 3 * 60 * 1000)); // Wait 3 minutes
+      await new Promise(resolve => setTimeout(resolve, 60 * 60 * 1000)); // Wait 3 minutes
     }
   } catch (err) {
     console.error(`Error in main loop: ${err.message}`);
+  } finally {
+    // Close database connection (assuming db has a close method)
+    await db.close();
   }
 }
 
-mainLoop();
+export default tomodatUpdateLoop;
