680b3573c202c16028e8b1c68940d43294dae0c1
sandrogaliazzi <sroque.roque@gmail.com>
2025-03-23 09:35:45 -0300

    alterado cors no backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,42 +1,50 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
-import db from "./config/dbConnect.js"
+import db from "./config/dbConnect.js";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 import reqMonitor from "./middleware/reqMonitor.js";
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
+const app = express();
 
-const app = express()
-
-db.on("error", console.log.bind(console, 'erro de conexão'))
-db.once('open', () => {
-   let now = new Date().toLocaleString("PT-br");
-    console.log(`conexão com o banco em: ${now}`)
-})
+db.on("error", console.log.bind(console, "erro de conexão"));
+db.once("open", () => {
+  let now = new Date().toLocaleString("PT-br");
+  console.log(`conexão com o banco em: ${now}`);
+});
 
 app.use(
   cors({
+    origin: "https://heatmap.conectnet.net",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+      "x-access-token",
+    ],
     credentials: true,
-  }),
-  //reqMonitor
-  );
+  })
+);
 
-app.get('/docapi', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get("/docapi", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
-app.get('/email', (req, res) => {
-  res.sendFile(path.join(__dirname, '/email.html'));
+app.get("/email", (req, res) => {
+  res.sendFile(path.join(__dirname, "/email.html"));
 });
-app.get('/lista-email', (req, res) => {
-  res.sendFile(path.join(__dirname, '/lista.html'));
+app.get("/lista-email", (req, res) => {
+  res.sendFile(path.join(__dirname, "/lista.html"));
 });
-app.use(express.json({limit: '50mb'})); //add {limit: '50mb'} referente ao erro PayloadTooLargeError: request entity too large
+app.use(express.json({ limit: "50mb" })); //add {limit: '50mb'} referente ao erro PayloadTooLargeError: request entity too large
 
 routes(app);
 
-export default app
+export default app;
