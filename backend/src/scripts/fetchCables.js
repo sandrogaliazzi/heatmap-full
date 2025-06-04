e5ecc8f1564ab1d0b47433140c66e7ff19b08b33
sandrogaliazzi <sroque.roque@gmail.com>
2025-06-04 16:15:54 -0300

    atualizado cabos do tomodat

--- backend/src/scripts/fetchCables.js
+++ backend/src/scripts/fetchCables.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-const filePath = path.join(__dirname, "/TOMODAT.kml");
+const filePath = path.join(__dirname, "/TOMODAT-NOVO.kml");
 
 function formatCoordinates(coordsArray) {
   const formatedCoordList = [];
